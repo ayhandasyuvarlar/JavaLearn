94e8f07bee99a43571ddcacc2febfc356943919f
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-07 21:33:47 +0300

    floatandDouble

---
+++ src/floatandouble.java
@@ -0,0 +1,14 @@
+public class floatandouble {
+    public static void main(String[]  args) {
+        float vFloat = 3.14f ;
+        System.out.println(vFloat);
+    }
+}
+ 
+/* Ondalık Sayı Veri Tiplerinden Hangisi Tercih Edilmelidir ?
+Bu sorunun cevabı değişken olmakla beraber hangi durumlarda Double veya Float kullanımınız için dikkat etmeniz gereken hususlar
+
+Double tipi, yüksek duyarlıklı ve hassas matematiksel işlemlerde kullanılır.
+Float basittir : hız ve bellek.
+Double daha yavaş ve fazla yer kaplar.(Bu işlemler arasında milisaniye oynar)
+Java hassas matematiksel işlemler, örneğin sinüs kosinüs fonksiyonları, double tipi değer döndürür. */
--- src/variables.java
+++ src/variables.java
@@ -1,5 +1,6 @@
 public class variables {
     public static void main(String[] args) {
+        // tam sayi veri tipleri 
         byte vByte = 127;
         short vShort = 1000;
         int vInt = 1000000000;
