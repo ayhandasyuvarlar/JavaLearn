9ab78bae03bbb40d988be29e7a30b31d44e81dcc
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-08 00:07:56 +0300

    input

---
+++ src/getData.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class getData {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b;
+
+        System.out.println("A sayısını giriniz : ");
+        a = input.nextInt();
+
+        System.out.println("B sayısını giriniz : ");
+        b = input.nextInt();
+
+        System.out.println("A Sayısı : " + a);
+        System.out.println("B Sayısı : " + b);
+    }
+}
