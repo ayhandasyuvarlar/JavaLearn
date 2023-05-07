1d6b0828cd67df0268c0b379a6f6d40ece9b5ad2
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-07 21:46:58 +0300

    charandboolean variables

---
+++ src/charandboolean.java
@@ -0,0 +1,16 @@
+public class charandboolean {
+  public static void main(String[] args) {
+    char vChar = 'v';
+    char VTwoChar = 98;
+    char c1 = 'J';
+    char c2 = 'A';
+    char c3 = 'V';
+    char c4 = 'A';
+    // System.out.println(vChar);
+    // System.out.println(VTwoChar);
+    // System.out.println("" + c1 + c2 + c3 + c4 );
+    boolean True = true;
+    boolean False = false;
+    System.out.println("" + True + " " + False);
+  }
+}
