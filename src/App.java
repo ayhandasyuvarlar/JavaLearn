5e60689d42903dbf06f7d40010742c1e223da816
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-07 20:15:57 +0300

    patika java

---
+++ src/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x, y, z;
+        x = 1;
+        y = 2;
+        z = x + y;
+        System.out.println(z);
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String[] args) {
+        byte vByte = 127;
+        short vShort = 1000;
+        int vInt = 1000000000;
+        long vLong = 1000000000;
+        System.out.println(vByte);
+        System.out.println(vShort);
+        System.out.println(vInt);
+        System.out.println(vLong);
+    }
+}
