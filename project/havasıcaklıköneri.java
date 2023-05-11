9770f16c89af8944f1ba81d87ced0cc676e34bbe
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-11 19:38:45 +0300

    havasıcaklıköneri  app

---
+++ project/havasıcaklıköneri.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class havasıcaklıköneri {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hava sicakligini giriniz : ");
+
+        heat = input.nextInt();
+        if (heat >= 25){
+            System.out.println("Yüzmeye gidebilirsin");
+        }else if(heat >= 10){
+            System.out.println("Piknige gidebilirsin");
+        }else if(heat>=5){
+            System.out.println("Sinemaya gidebilirsin");
+        }else if(heat>=0){
+            System.out.println("Kayak Yapabilirsin");
+        }
+    }
+}
