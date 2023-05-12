68583d4c700a114ee5c08cd0fb7a844377c54652
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-12 15:55:35 +0300

    while loop

---
+++ src/whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int k = 1;
+        int i = 10;
+        while (k < i) {
+            System.out.println(k + 1);
+            k++;
+        }
+    }
+}
