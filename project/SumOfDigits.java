e66476edf6be10788f5621163e3ae2cdef02ffab
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-13 18:42:58 +0300

    Sum of digits

---
+++ project/SumOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] Args) {
+
+        Scanner inp = new Scanner(System.in);
+        int number, remain, total = 0;
+        System.out.print("Enter a number: ");
+        number = inp.nextInt();
+
+        while (number != 0) {
+            remain = number % 10;
+            total += remain;
+            number /= 10;
+        }
+        System.out.print(total);
+    }
+}
