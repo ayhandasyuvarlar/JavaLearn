ec7f5cbe3eb1316a723a0876fff2efc732b9e2fa
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-13 19:56:13 +0300

    rename

--- project/tektoplam.java
+++ project/tektoplam.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class tektoplam {
+public class singleTotal {
     public static void main(String[] args) {
         int num;
         int total = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bir numara giriniz");
+        System.out.println("Enter a number");
         num = sc.nextInt();
 
         for (int i = 0; i < num; i++) {
