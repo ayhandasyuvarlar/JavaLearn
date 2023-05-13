2a0fcfc20eea689be24b75ad30224ad6d180907a
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-13 20:14:21 +0300

    invertedTriangle

---
+++ project/invertedTriangle.java
@@ -0,0 +1,20 @@
+public class invertedTriangle {
+    public static void main(String[] args) {
+        int ix,j,kx;
+        for (ix=8; 0<ix; ix--) {
+            for(j=ix; j<8;j++) {
+                System.out.print(" ");
+            for (kx=1; kx<=ix; kx++) {
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+
+
+        }
+
+
+
+    }
+
+}}
